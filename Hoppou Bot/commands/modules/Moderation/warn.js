a4fddd8d64e638250f50357dd76d2b8bddd79546
zander123p <zrashid2001@gmail.com>
2020-12-06 13:20:57 +0000

    Added base support for warning a user

---
+++ Hoppou Bot/commands/modules/Moderation/warn.js
@@ -0,0 +1,34 @@
+module.exports = {
+    name: 'warn',
+    description: 'Warns the user provided.',
+    guildOnly: true,
+    permissions: ['KICK_MEMBERS'],
+    guildPermission: 'mod.warn',
+    args: 2,
+    usage: '<user> <reason>',
+    async execute(message, args) {
+        const target = await message.getUserFromID(args[0]);
+        console.log(target);
+        const guildTarget = message.guild.members.cache.find(u => target.id === u.id);
+        console.log(guildTarget);
+        const reason = args.slice(1).join(' ');
+        const userProfile = await guildTarget.user.ensure();
+        const mg = require('mongoose');
+        const newActionId = mg.Types.ObjectId();
+        const log = new message.client.ActionLogs({
+            _id: newActionId,
+            userID: guildTarget.id,
+            guildID: guildTarget.guild.id,
+            type: 'warning',
+            moderator: message.author.id,
+            reason: reason
+        });
+        await log.save();
+        userProfile.warnings.push(newActionId);
+        userProfile.totalActions += 1;
+        await userProfile.save();
+        if (!target.bot)
+            target.send(`You have been warned by ${message.author} for ${reason}!`);
+        message.channel.send(`${target} was warned for \'${reason}\'`);
+    },
+};
