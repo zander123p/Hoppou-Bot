d25013e913e58b4e63bc6f1c12aa7e34a1750127
zander123p <zrashid2001@gmail.com>
2021-01-24 16:48:08 +0000

    Added total message counter.

--- Hoppou Bot/models/guild_user.js
+++ Hoppou Bot/models/guild_user.js
@@ -5,6 +5,7 @@ const user_profileSchema = mongoose.Schema({
     guildID: String,
     userID: String,
     permissionGroups: [String],
+    messages: Number,
 });
 
 module.exports = mongoose.model('Guild User', user_profileSchema, 'guild_users');
