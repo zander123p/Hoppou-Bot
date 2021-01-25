8af5a088851dd21ac0a4d544f74ebb4286be8944
zander123p <zrashid2001@gmail.com>
2021-01-25 14:11:05 +0000

    Finally updated to use embed and also added a secondary function to see a bit more info on a specific log.

--- Hoppou Bot/commands/modules/Administration/listlogs.js
+++ Hoppou Bot/commands/modules/Administration/listlogs.js
@@ -4,23 +4,53 @@ module.exports = {
     guildOnly: true,
     permissions: ['ADMINISTRATION'],
     guildPermission: 'admin.listlogs',
+    usage: '[log]',
     async execute(message, args) {
+        const ListedEmbed = require('../../../utils/listedembed');
         const guild = await message.guild.ensure();
         // let list = message.client.events.map(c => { return c.logs.map(l => { return l.toString() + '\n'; }).toString(); });
         let activeLogs = [];
         guild.settings.channels.forEach(c => {
             c.logs.forEach(l => {
-                activeLogs.push(l);
+                activeLogs.push({log: l, channel: c.name});
             });
         });
-        let list = '';
-        Array.from(message.client.events.keys()).forEach(k => {
-            if (activeLogs.includes(message.client.events.get(k))) {
-                list += k + ': ' + message.client.events.get(k) + '|ACTIVE\n';
+
+        let log = args[0];
+        if (log) {
+            log = log.toLowerCase();
+            if (Array.from(message.client.events.values()).map(x => x.toLowerCase()).includes(log)) {
+                const embed = new ListedEmbed()
+                    .setColor('#9a3deb')
+                    .setTitle(`Logs - ${message.client.events.find(x => x.toLowerCase() === log)[0].toUpperCase() + message.client.events.find(x => x.toLowerCase() === log).substring(1)}`);
+                
+                let actLog = activeLogs.find(x => x.log.toLowerCase() === log);
+                if (actLog) {
+                    embed.addField(`State`, `ACTIVE`);
+                    embed.addField(`Channel`, message.guild.channels.cache.get(actLog.channel));
+                } else {
+                    embed.addField(`State`, `INACTIVE`);
+                }
+                embed.send(message.channel, 5);
             } else {
-                list += k + ': ' + message.client.events.get(k) + '\n';
+                message.reply('please enter a valid log.').then(msg => msg.delete({ timeout: 5000 }));
+                return message.react('❌');
+            }
+            return;
+        }
+
+        const embed = new ListedEmbed()
+            .setColor('#9a3deb')
+            .setTitle('Logs');
+
+        Array.from(message.client.events.keys()).forEach(k => {
+            if (activeLogs.find(x => x.log === message.client.events.get(k))) {
+                embed.addField(message.client.events.get(k)[0].toUpperCase() + message.client.events.get(k).substring(1), 'ACTIVE');
+            } else {
+                embed.addField(message.client.events.get(k)[0].toUpperCase() + message.client.events.get(k).substring(1), 'INACTIVE');
             }
         });
-        message.channel.send(`Here is the log id list: \`\`\`${list}\`\`\``);
+
+        embed.send(message.channel, 5);
     }
 }
