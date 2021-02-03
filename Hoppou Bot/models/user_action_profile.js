429d705aa7e54cad6a5863aef46266d4d9ef8add
zander123p <zrashid2001@gmail.com>
2021-02-03 10:18:33 +0000

    Moved old user profile to new model and changed old model to suite a general profile system.

---
+++ Hoppou Bot/models/user_action_profile.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const user_profileSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    userID: String,
+    totalActions: Number,
+    mutes: Number,
+    warnings: [String],
+    kicks: [String],
+    bans: [String],
+});
+
+module.exports = mongoose.model('User Action Profile', user_profileSchema, 'user_action_profiles');
