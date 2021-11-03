d16bcc1544130384ff47d89b39908f40f0fc96e6
zander123p <zrashid2001@gmail.com>
2021-11-03 09:57:38 +0000

    User verification logs

---
+++ Hoppou Bot/Modules/Logs/userVerificationReject.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'User Rejected',
+    eventType: 'userVerificationReject',
+    async event(client, member, moderator) {
+        const { MessageEmbed } = require('discord.js');
+        const { userMention } = require('@discordjs/builders');
+
+        const logs = await member.guild.getModuleSetting(this.module, 'logs');
+        const log = logs.find(l => l.logs.includes(this.name.toLowerCase()));
+        if (!log) return;
+        const c = member.guild.channels.cache.get(log.id);
+
+        const meU = new MessageEmbed()
+            .setColor('#70f567')
+            .setTitle('User Rejected')
+            .setAuthor(moderator.user.tag, moderator.user.displayAvatarURL())
+            .addField('User', userMention(member.user.id))
+            .setTimestamp();
+        c.send({ embeds: [meU] });
+    },
+};
