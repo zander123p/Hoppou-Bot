5fe286482b8c5771f68944435d0875ba5e2e47c8
zander123p <zrashid2001@gmail.com>
2021-11-05 10:28:47 +0000

    Removed guild member kick emit

--- Hoppou Bot/Modules/Logs/guildMemberRemove.js
+++ Hoppou Bot/Modules/Logs/guildMemberRemove.js
@@ -4,19 +4,11 @@ module.exports = {
     async event(client, member) {
         const { MessageEmbed } = require('discord.js');
         const { userMention } = require('@discordjs/builders');
-        const g = await member.guild.ensure();
 
         const logs = await member.guild.getModuleSetting(this.module, 'logs');
         const log = logs.find(l => l.logs.includes(this.name.toLowerCase()));
         if (!log) return;
         const c = member.guild.channels.cache.get(log.id);
-
-        const fetchedLogs = await member.guild.fetchAuditLogs({
-            limit: 1,
-            type: 'MEMBER_KICK',
-        });
-
-        const channelLog = fetchedLogs.entries.first();
 
         const me = new MessageEmbed()
             .setColor('#db4444')
@@ -25,24 +17,6 @@ module.exports = {
             .addField('Member', userMention(member.user.id))
             .setTimestamp();
 
-        if (!channelLog) return c.send({ embeds: [me] });
-
-        const oldLog = g.oldLogs.find(C => { if(channelLog.id === C.id) return C; });
-        if (oldLog) return c.send({ embeds: [me] });
-
-        const pos = g.oldLogs.findIndex(C => { if(C.log === this.name.toLowerCase()) return C; });
-        if (pos < 0) {
-            g.oldLogs.push({ id: channelLog.id.toString(), log: this.name.toLowerCase() });
-        } else {
-            g.oldLogs[pos].id = channelLog.id.toString();
-        }
-
-        await g.save();
-
-        client.emit('guildMemberKick', member);
-
         c.send({ embeds: [me] });
-
-        await client.GuildUsers.findOneAndDelete({ userID: member.user.id, guildID: member.guild.id });
     },
 };
