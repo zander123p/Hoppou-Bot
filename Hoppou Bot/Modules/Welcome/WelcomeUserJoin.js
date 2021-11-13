b063a6f871977f9cd7bb8aaf031927a37ac59158
zander123p <zrashid2001@gmail.com>
2021-11-13 19:42:43 +0000

    Fixed event not firing correctly due to it being the wrong event

--- Hoppou Bot/Modules/Welcome/WelcomeUserJoin.js
+++ Hoppou Bot/Modules/Welcome/WelcomeUserJoin.js
@@ -1,19 +1,19 @@
 module.exports = {
-    eventType: 'guildMemberUpdate',
-    async event(client, oldMember, newMember) {
-        const chanl = await newMember.guild.getModuleSetting(this.module, 'welcome_channel');
-        let msg = await newMember.guild.getModuleSetting(this.module, 'welcome_message');
-        const joinFlag = await newMember.guild.getModuleSetting(this.module, 'join_flag');
+    eventType: 'guildMemberAdd',
+    async event(client, member) {
+        const chanl = await member.guild.getModuleSetting(this.module, 'welcome_channel');
+        let msg = await member.guild.getModuleSetting(this.module, 'welcome_message');
+        const joinFlag = await member.guild.getModuleSetting(this.module, 'join_flag');
         if (!chanl || !msg || !joinFlag) {
             return;
         }
 
-        const channel = oldMember.guild.channels.cache.get(chanl);
+        const channel = member.guild.channels.cache.get(chanl);
 
-        msg = msg.replaceAll('{{user}}', oldMember);
-        msg = msg.replaceAll('{{server}}', oldMember.guild);
+        msg = msg.replaceAll('{{user}}', member);
+        msg = msg.replaceAll('{{server}}', member.guild);
         // const memberCount = newMember.guild.roles.cache.get(welcomeRole).members.size;
-        const memberCount = newMember.guild.memberCount;
+        const memberCount = member.guild.memberCount;
         msg = msg.replaceAll('{{count}}', `${memberCount}${([4, 5, 6, 7, 8, 9, 0].includes(memberCount % 10)) ? 'th' : (([1].includes(memberCount % 10)) ? 'st' : (([2].includes(memberCount % 10)) ? 'nd' : 'rd'))}`);
         msg = msg.replaceAll('{{line}}', '\n');
 
