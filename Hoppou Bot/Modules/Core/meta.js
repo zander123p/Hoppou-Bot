9231a04cb622896b91e7a46651255e84775e57f3
zander123p <zrashid2001@gmail.com>
2021-11-13 19:42:10 +0000

    Typo

--- Hoppou Bot/Modules/Core/meta.js
+++ Hoppou Bot/Modules/Core/meta.js
@@ -8,7 +8,7 @@ module.exports = {
         'GuildLeave',
         'Interaction',
         'MessageSent',
-        'KickEmitter',
+        'KickEmiter',
         'MessageOnBan',
         'MessageOnKick',
     ],
